59ae4631f0eb3b6b527348220d70fca704fc2ec2
AyeChan08 <ayechan.mon@digipowermm.com>
2024-05-20 11:53:47 +0630

    change model

--- lib/models/document_employee.dart
+++ lib/models/document_employee.dart
@@ -1,16 +1,71 @@
+// import 'dart:convert';
+
+// class DocumentEmployee {
+//   int id;
+//   String name;
+//   DocumentEmployee({
+//     this.id,
+//     this.name,
+//   });
+
+//   DocumentEmployee copyWith({
+//     int id,
+//     String name,
+//   }) {
+//     return DocumentEmployee(
+//       id: id ?? this.id,
+//       name: name ?? this.name,
+//     );
+//   }
+
+//   Map<String, dynamic> toMap() {
+//     return {
+//       'id': id,
+//       'name': name,
+//     };
+//   }
+
+//   factory DocumentEmployee.fromMap(Map<String, dynamic> map) {
+//     if (map == null) return null;
+
+//     return DocumentEmployee(
+//       id: map['id'],
+//       name: map['name'],
+//     );
+//   }
+
+//   String toJson() => json.encode(toMap());
+
+//   factory DocumentEmployee.fromJson(String source) =>
+//       DocumentEmployee.fromMap(json.decode(source));
+
+//   @override
+//   String toString() => 'DocumentEmployee(id: $id, name: $name)';
+
+//   @override
+//   bool operator ==(Object o) {
+//     if (identical(this, o)) return true;
+
+//     return o is DocumentEmployee && o.id == id && o.name == name;
+//   }
+
+//   @override
+//   int get hashCode => id.hashCode ^ name.hashCode;
+// }
+
 import 'dart:convert';
 
 class DocumentEmployee {
-  int id;
-  String name;
+  final int id;
+  final String name;
   DocumentEmployee({
-    this.id,
-    this.name,
+    this.id = 0,
+    this.name = '',
   });
 
   DocumentEmployee copyWith({
-    int id,
-    String name,
+    int? id,
+    String? name,
   }) {
     return DocumentEmployee(
       id: id ?? this.id,
@@ -26,7 +81,7 @@ class DocumentEmployee {
   }
 
   factory DocumentEmployee.fromMap(Map<String, dynamic> map) {
-    if (map == null) return null;
+    // if (map == null) return null;
 
     return DocumentEmployee(
       id: map['id'],
